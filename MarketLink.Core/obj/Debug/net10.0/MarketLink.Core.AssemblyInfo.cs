64b4ababfeb89c2edaf5c2d5d139bafb8ade28a4
FaakhirMemon3115 <alexwatson031105@gmail.com>
2026-09-23 04:37:18 -0700

    Core.dil marketLink

--- MarketLink.Core/obj/Debug/net10.0/MarketLink.Core.AssemblyInfo.cs
+++ MarketLink.Core/obj/Debug/net10.0/MarketLink.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarketLink.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+defe061bf149310f65faaaf3025cc24bcd6aa150")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c833b584ffa8bf8f769f65cba09f77f7f6709698")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarketLink.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarketLink.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
