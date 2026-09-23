8fd9c05ba242a0b636e0b9f15ab399a8d78ebc85
FaakhirMemon3115 <alexwatson031105@gmail.com>
2026-09-23 11:30:43 -0700

    all over

--- MarketLink.Infrastructure/obj/Debug/net10.0/MarketLink.Infrastructure.AssemblyInfo.cs
+++ MarketLink.Infrastructure/obj/Debug/net10.0/MarketLink.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarketLink.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f9878af81a80c8c41393ef0ca50a6e29e11f2de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf5d3e6fdcb069e8f7f56f28d3dc100ffc26077a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarketLink.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarketLink.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MarketLink.Web/obj/Debug/net10.0/MarketLink.Web.AssemblyInfo.cs
+++ MarketLink.Web/obj/Debug/net10.0/MarketLink.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarketLink.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f9878af81a80c8c41393ef0ca50a6e29e11f2de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf5d3e6fdcb069e8f7f56f28d3dc100ffc26077a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarketLink.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarketLink.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
