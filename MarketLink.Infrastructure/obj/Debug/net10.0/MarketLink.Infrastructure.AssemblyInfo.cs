0c6a3092a05b2319c1c7e4134f553c0071682634
FaakhirMemon3115 <alexwatson031105@gmail.com>
2026-09-24 09:40:02 -0700

    addon some integration

--- MarketLink.Infrastructure/obj/Debug/net10.0/MarketLink.Infrastructure.AssemblyInfo.cs
+++ MarketLink.Infrastructure/obj/Debug/net10.0/MarketLink.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarketLink.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+393c79082a89d66806b018a9d4f480b0150813fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c25d5bb6e72ae723d6fa517f2482dc6af937ffa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarketLink.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarketLink.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
